354593d4190e73ee365ce5cae15cae2f3defccad
test <test@224_TEACHER_>
2018-04-16 19:50:05 +0600

    Add first class called Human, teaching purposes

--- SEP-172.2/Source.cpp
+++ SEP-172.2/Source.cpp
@@ -1,5 +1,27 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class Human 
+{
+public:
+	//state
+	int age;
+	string name;
+
+	//operations
+	void hello() {
+		cout << "Hello, I am: " << name << endl;
+	}
+};
 
 int main() {
-
+	Human h1, h2;
+	h1.name = "LOL";
+	h2.name = "QWERTY";
+	h1.hello();
+	h2.hello();
+	system("pause");
 	return 0;
 }
