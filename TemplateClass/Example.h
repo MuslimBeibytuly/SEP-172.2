401b779e7bd2ca39c8f0f1c8fef29a98d2a5045a
test <test@224_TEACHER_>
2018-05-10 20:57:44 +0600

    array

---
+++ TemplateClass/Example.h
@@ -0,0 +1,13 @@
+#pragma once
+class Example
+{
+public:
+	int x, y;
+	Example(int x, int y) {
+		this->x = x;
+		this->y = y;
+	}
+	Example();
+	~Example();
+};
+
--- TemplateClass/Source.cpp
+++ TemplateClass/Source.cpp
@@ -2,20 +2,45 @@
 #include <vector>
 #include <array>
 #include <memory>
+#include "Example.h"
+template <class ... T>
+bool isInRadius(T ... & points)
+{
+	int r = 5;
+	for (
+		auto && point : std::initializer_list<Example>({ points... })
+		)
+	{
+		if (
+			point.x * point.x +
+			point.y * point.y <= r * r
+		)
+		{
+			std::cout << 
+				point.x << ' ' << point.y << std::endl;
+			return true;
+		}
+	}
+	return false;
+}
 void sample()
 {
 	//std::array<std::array<int, 5>, 5> a;
 }
-
 template <class T1, class ... T2>
-void print(T1 t1, T2 ... t2) 
+void print(T1 t1, T2 ... t2)
 {
 	std::cout << t1 << std::endl;
 	print(t2...);
 }
-void print(){}
+void print() {}
 void main()
 {
+	std::cout << isInRadius(
+		Example{ 7, 7 }, Example{ 9, 9 },
+		Example{ 1, 2 }, Example{ 2, 3 }
+	);
+	std::vector<int> x;
 	print(1, '2', "vfndk", 4, 2.4, 100L);
 	system("pause");
 }
