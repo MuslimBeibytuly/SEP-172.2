13f4f643f617544b34c3f422777b269ce5fd6fb6
test <test@224_TEACHER_>
2018-06-13 20:01:09 +0600

    Add SFML

--- SnakeGame/Source.cpp
+++ SnakeGame/Source.cpp
@@ -1,5 +1,32 @@
 #include "SnakeGame.h"
-int main() {
-	SnakeGame::getInstance().play();
+#include <SFML\Graphics.hpp>
+int main() 
+{
+	sf::RenderWindow window(sf::VideoMode(1600, 900), "SFML works!");
+	sf::RectangleShape shape(sf::Vector2f(200, 200));
+	shape.move(sf::Vector2f(200, 200));
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+			else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) 
+			{
+				shape.move(sf::Vector2f(-200, 0));
+			}
+			else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) 
+			{
+				shape.move(sf::Vector2f(200, 0));
+			}
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
 	return 0;
 }
